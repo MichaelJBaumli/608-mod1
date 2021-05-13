09c79c71ddac1f5a7547795eb064b5172292e512
Michael J Baumli <michaeljbaumli@gmail.com>
2021-05-12 21:38:29 -0500

    Added Bonus Content

---
+++ bonus.py
@@ -0,0 +1,20 @@
+#Program Name: min.py
+#Assignment Module 1
+#Class 44680 Block 44599 Section 01
+#Michael Baumli
+#Date: 20210512
+
+testrange =[43,62,76,74,66,68,24,89,94,74,99]
+
+
+min(testrange)
+print('min(',testrange,')')
+print('min:',min(testrange))
+
+max(testrange)
+print('max(',testrange,')')
+print('max:',max(testrange))
+
+#range(testrange)
+print('Range: ',min(testrange), '-', 
+max(testrange))
